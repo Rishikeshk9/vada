9932beea4990c46ed9d07aeffecb131c3ba06fe3
Rishikeshk9 <rushikeshkardile9@gmail.com>
2022-07-12 03:33:43 +0530

    Added Chat Functions

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "xmtp-blue": "#00bcd4",
+        "xmtp-green": "#4caf50",
+        "xmtp-red": "#f44336",
+        "xmtp-yellow": "#ffeb3b",
+        "xmtp-orange": "#ff9800",
+        "xmtp-purple": "#9c27b0",
+        "xmtp-pink": "#e91e63",
+        "xmtp-brown": "#795548",
+        "xmtp-grey": "#9e9e9e",
+        "xmtp-white": "#ffffff",
+        "xmtp-black": "#000000",
+        "xmtp-dark-grey": "#616161",
+        "xmtp-dark-blue": "#0277bd",
+        "xmtp-dark-green": "#388e3c",
+      },
+    },
+  },
+  plugins: [],
+};
